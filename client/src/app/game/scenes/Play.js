17233cace1f4674f925ba5d376d2d2ce8dd7959b
arnol <arnolfokam23@gmail.com>
2019-09-07 21:10:17 +0100

    code refactoring an bug fixes

--- client/src/app/game/scenes/Play.js
+++ client/src/app/game/scenes/Play.js
@@ -6,10 +6,10 @@ import * as Colyseus from "colyseus.js";
 
 
 
-//const endpoint = (window.location.hostname === "localhost") ? `ws://localhost:${process.env.PORT}` : `${window.location.protocol.replace("http", "ws")}//${window.location.hostname}:${process.env.PORT}`;
+const endpoint = (window.location.hostname === "localhost") ? `ws://localhost:${process.env.PORT}` : `${window.location.protocol.replace("http", "ws")}//${window.location.hostname}:${process.env.PORT}`;
 
 //for heroku remote deployment...to run it locally comment the code below and uncomment the code at the top
-const endpoint = (window.location.protocol === "http:") ? `ws://${process.env.APP_URL}` : `wss://${process.env.APP_URL}`
+//const endpoint = (window.location.protocol === "http:") ? `ws://${process.env.APP_URL}` : `wss://${process.env.APP_URL}`
 
 
 
